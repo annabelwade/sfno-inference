b5bbd0fecd71188e05fd49c28e1209fee08b1896
annabelwade <wade@bu.edu>
2026-03-25 13:07:32 -0400

    add batch job script without job array/non-parallelized

---
+++ inference.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+
+# Set the time limit
+#$ -l h_rt=04:00:00
+
+# Name the job
+#$ -N inference
+
+# Request 1 GPU 
+#$ -l gpus=1
+#$ -l gpu_c=7.0  
+
+# Request 4 cores, each with 8GB memory
+#$ -pe omp 4
+#$ -l mem_per_core=8G
+
+# Explicitly discard SGE system log, because we are redirecting output to a custom log file in the script
+#$ -o /dev/null
+
+# Merge output and error files
+#$ -j y
+
+#$ -P PROJECT_NAME          # Specify the SCC project name you want to use
+#$ -m ea                    # Send email when job ends or aborts
+
+# Get the experiment number from the command line argument
+EXP_NUM=$1
+
+# Define the log directory and file name
+LOG_DIR="INSERT_LOG_DIRECTORY/Experiment${EXP_NUM}"
+LOG_FILE="${LOG_DIR}/inference_parallel_${JOB_ID}.log"
+
+# Create the directory if it doesn't exist
+mkdir -p $LOG_DIR
+
+exec > $LOG_FILE 2>&1
+
+# Environment Setup
+module load miniconda 
+conda activate earth2studio
+
+cd INSERT_PROJECT_DIRECTORY
+python ./inference.py $EXP_NUM
